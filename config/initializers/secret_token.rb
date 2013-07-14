3a208bcd4b4e85eda2ca0ad3b8588c635f8d90d0
Andy Pickler <andy.pickler@gmail.com>
2013-07-14 10:03:24 -0600

    Initial commit of empty application with core Gems needed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '6b327d410c3f2c0a041bb14eb3afaab1e20e120f9db4ed7f0155efd75d813c27d9d5be811177b12859de92f1d46efbe63865efc721e5b29c0f6ec9da52e1bc6c'
